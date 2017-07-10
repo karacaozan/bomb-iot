6ce1a8b9c1fcc783362b34c90ecd655540653e00
karacaozan <ben@ozankaraca.com>
2017-07-10 11:47:42 +0300

    Update script.js

--- data/script.js
+++ data/script.js
@@ -1,1 +1,18 @@
 var v = "Hello";
+
+function checkNetConnection(){
+ var xhr = new XMLHttpRequest();
+ var file = "http://yoursite.com/somefile.png";
+ var r = Math.round(Math.random() * 10000);
+ xhr.open('HEAD', file + "?subins=" + r, false);
+ try {
+  xhr.send();
+  if (xhr.status >= 200 && xhr.status < 304) {
+   return true;
+  } else {
+   return false;
+  }
+ } catch (e) {
+  return false;
+ }
+}
