058b08e314764ca00eb50000da6d55cbd0becf83
karacaozan <ben@ozankaraca.com>
2017-07-25 00:42:39 +0300

    Add files via upload

--- script_add.js
+++ script_add.js
@@ -65,8 +65,8 @@ function AddRemote(sender) {
     if (_devices[index].type && _devices[index].type == "RF433Switch") {
 
         accessory = RF433Switch(remoteAccessory.id, remoteAccessory.name, "RF433Switch", false, "Off", remoteAccessory.ip);
-        accessory.SetActionData("On", commandOnValue.value);
-        accessory.SetActionData("Off", commandOffValue.value);
+        accessory.SetActionData("Off", RF433Switch.actions[0].data);
+        accessory.SetActionData("On", RF433Switch.actions[1].data);
     }
     else if (_devices[index].type && _devices[index].type == "TemperatureSensor") {
         accessory = TemperatureSensor(remoteAccessory.id, remoteAccessory.name, remoteAccessory.icon, false, "Off", remoteAccessory.ip);
@@ -74,7 +74,7 @@ function AddRemote(sender) {
 
     else if (_devices[index].type && _devices[index].type == "HTTPWebRequest") {
         accessory = HTTPWebRequest(remoteAccessory.id, remoteAccessory.name, "sensor", false, "Get", remoteAccessory.ip);
-        accessory.SetActionData("Callback", HTTPWebRequestCallback.data);
+        accessory.SetActionData("Callback", HTTPWebRequestCallback.actions[0].data);
     }
 
 
